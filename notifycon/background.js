f75cb190b72e1ebe12a986a26df9378670d84208
justin <jpaglier@asu.edu>
2016-06-23 09:52:49 -0400

    rewrite to use listener pattern, fix sticking notification bug, code cleanup

--- notifycon/background.js
+++ notifycon/background.js
@@ -1,5 +1,7 @@
+"use strict";
+
 var o = new Rain1Overlay("background");
-var ids = {};
+var icons = {};
 var alreadyReloaded = {};
 var disabledDomains = [];
 
@@ -18,78 +20,21 @@ function setDisabledDomains(r) {
 	disabledDomains = r;
 }
 
-var _f = function(first) {
-	chrome.tabs.query({pinned:true}, function (tabs) {
-		for(var i = 0; i<tabs.length; i++) {
-			for(var j = 0; j<disabledDomains.length; j++) {
-				if(tabs[i].url.indexOf(disabledDomains[j])>=0) {
-					return ;
-				} 
-			}
-			// first time to see this tab. Get the favicon and store it
-			if(typeof ids[tabs[i].id] == "undefined") {
-				// Don't take my icon if the extension is reloaded
-				if(!tabs[i].favIconUrl)
-					continue;
-				if(tabs[i].favIconUrl.split("data:image/png").length>1) {
-					// if never reloaded
-					if(typeof alreadyReloaded[tabs[i].id] == "undefined") {
-						chrome.tabs.reload(tabs[i].id);
-						alreadyReloaded[tabs[i].id] = true;
-						continue;
-					}
-				}
-				ids[tabs[i].id] = {};
-				ids[tabs[i].id].tab = tabs[i];
-				ids[tabs[i].id].originalIcon = tabs[i].favIconUrl;
-				ids[tabs[i].id].oldN = getNumberFromTitle(tabs[i].title);
-
-				var img = new Image();
-				img.onload = (function(uid,img) {
-					var cImg = img;
-					ids[uid].dataIcon = cImg; 
-					return function() {
-						ids[uid].dataIcon = cImg; 
-						if(ids[uid].oldN != 0) {
-							var base64 = createIcon(cImg,ids[uid].oldN);
-							injectFavicon(uid,base64);
-						}
-					}
-				})(tabs[i].id,img);
-				img.src = tabs[i].favIconUrl;
-			}else {
-				var newNumber = getNumberFromTitle(tabs[i].title);
-				if(newNumber != ids[tabs[i].id].oldN) {
-					var base64 = createIcon(ids[tabs[i].id].dataIcon,newNumber);
-					ids[tabs[i].id].oldN = newNumber;
-					injectFavicon(tabs[i].id,base64);
-				}
-			}
-			
-
-
-			
-		}
-		setTimeout(_f,2000);
-	});
-};
-
-function injectFavicon(tabId,base64) {
+function injectFavicon(tab,base64) {
 	
-	var code = '(function() {'+
+	let code = '(function() {'+
 	'var notLink = document.querySelector("#notifycon"); '+
 	'if(!notLink) {'+
 	'var link, oldLink, '+
 	'links = document.querySelectorAll("link[rel=\'icon\'],link[rel=\'shortcut icon\']");'+"\n"+
-	'for(var i =0; i<links.length; i++) {'+"\n"+
-	'   links[i].parentNode.removeChild(links[i])'+
+	'for(let link of links) {'+"\n"+
+	'   link.setAttribute(\'rel\', \'icon_old\');'+
 	'}'+"\n"+
 	'var link = document.createElement("LINK");'+"\n"+
 	'link.rel = "icon";'+"\n"+
 	'link.type="image/x-icon";'+"\n"+					
 	'link.href = "'+base64+'";'+"\n"+
 	'link.id = "notifycon";'+"\n"+
-	'link.dataset.originalIcon = "";'+"\n"+	
 	
 	'document.head.appendChild(link);'+"\n"+
 	'}'+
@@ -97,12 +42,12 @@ function injectFavicon(tabId,base64) {
 	'notLink.href = "'+base64+'";'+"\n"+
 	'}'+
 	'})()';
-	console.log("Icon of " + ids[tabId].tab.url+ " changed to " + ids[tabId].oldN);
-	console.log(ids[tabId].tab);
-	console.log(ids[tabId].tab.InjectDetails);
+	console.log("Icon of " + tab.url+ " changed to " + icons[tab.id].oldN);
+	console.log(tab);
+	console.log(tab.InjectDetails);
 
 	
-	chrome.tabs.executeScript(tabId, {code:code}, function() {
+	chrome.tabs.executeScript(tab.id, {code:code}, function() {
 		console.log("icona cambiata");
 	})
 	
@@ -111,14 +56,14 @@ function injectFavicon(tabId,base64) {
  * public
  */
 function createIcon(imgIco,nText) {
-	var canvas = document.createElement('canvas');
+	let canvas = document.createElement('canvas');
 	canvas.height = canvas.width = 16; // set the size
 	
-	var ctx = canvas.getContext('2d');
+	let ctx = canvas.getContext('2d');
 
 	if(nText != 0 && nText != "") {
-		var len = Math.min(3,nText.toString().length);
-		var startX = 10-(len-1)*5;
+		let len = Math.min(3,nText.toString().length);
+		let startX = 10-(len-1)*5;
 		ctx.drawImage(imgIco, 0, -2, 16,16);
 		
 		ctx.rect(startX-2, 8, 30, 14);
@@ -127,17 +72,17 @@ function createIcon(imgIco,nText) {
 		ctx.fillStyle = 'red';
 		ctx.strokeStyle = 'black'
 		ctx.shadowColor = '#000';
-	    ctx.fill();
+		ctx.fill();
 		
 		ctx.font = 'bold 9px sans-serif';
 		ctx.fillStyle = '#fff';
 		/*
 		ctx.shadowColor = "#000";
-	    ctx.shadowOffsetX = 0;
-	    ctx.shadowOffsetY = 0;
-	    ctx.shadowBlur = 1;
-	    */
-	    ctx.fillText(nText, startX, 15);
+		ctx.shadowOffsetX = 0;
+		ctx.shadowOffsetY = 0;
+		ctx.shadowBlur = 1;
+		*/
+		ctx.fillText(nText, startX, 15);
 
 	}else {
 		return imgIco.src;
@@ -147,20 +92,78 @@ function createIcon(imgIco,nText) {
 }
 
 function getNumberFromTitle(title) {
-	var mtch = title.match(/\(([0-9]+)[^0-9]*?\)/) || [];
+	let mtch = title.match(/\(([0-9]+)[^0-9]*?\)/) || [];
 	if(mtch.length>1) {
 		return  mtch[1];
 	}
 	return 0;
 }
 
-_f();
-chrome.tabs.onHighlighted.addListener(function(highlightInfo) {
-	//console.log(highlightInfo.tabIds);
-	//_f();
-});
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-//	if(changeInfo.status=="complete" && ids[tabId])
-//		ids[tabId].oldN = 0;
-});
+// return domain name - jpaglier
+function get_domain_from_url(url) {
+	let a = document.createElement('a');
+	a.setAttribute('href', url);
+	return a.hostname;
+}
 
+function processTab(tab) {
+	// simplify the disabledDomains search
+	let domain = get_domain_from_url(tab.url);
+	if(disabledDomains.indexOf(domain) >= 0) return;
+
+	// first time to see this tab. Get the favicon and store it
+	if(icons[tab.id] === undefined) {
+		// Don't take my icon if the extension is reloaded
+		if(!tab.favIconUrl) return;
+		// prevent using a notifycon as base - fix stuck notification bug jpaglier
+		if(tab.favIconUrl.indexOf('http') < 0)  return;
+
+		icons[tab.id] = {};
+		icons[tab.id].oldN = getNumberFromTitle(tab.title);
+		icons[tab.id].dataIcon = getImgData(tab.favIconUrl);
+	}
+
+	// remove some code duplication - simplify codepath - jpaglier
+	let newNumber = getNumberFromTitle(tab.title);
+	if(newNumber != icons[tab.id].oldN) {
+		let base64 = createIcon(icons[tab.id].dataIcon,newNumber);
+		icons[tab.id].oldN = newNumber;
+		injectFavicon(tab,base64);
+	}
+}
+
+// get image data - abstracted by jpaglier
+function getImgData(url) {
+	let img = new Image();
+	img.src = url;
+	return img.onload = (function(img) {
+		return img;
+	})(img);
+
+}
+
+// listeners are better - jpaglier
+function createdListener(tab) {
+	if(tab.pinned) processTab(tab);
+}
+
+function updatedListener(tabId, changeInfo, tab) {
+	if(changeInfo['title'] && tab.pinned) processTab(tab); 
+}
+
+// much simple main() - jpaglier
+var _f = function(first) {
+	chrome.tabs.onCreated.addListener(createdListener);
+	chrome.tabs.onUpdated.addListener(updatedListener);
+	// do best to remove old notifycon favicons
+	chrome.tabs.query({'pinned':true}, function (tabs) {
+			for(let tab of tabs) {
+//                                removeFavicons(tab);
+                                chrome.tabs.reload(tab.id);
+			}
+		}
+	);
+
+};
+
+_f();
